f16e82ef8261b149a728740a267667875da33179
Chris McLeod <mcleod32@live.com>
2022-03-07 17:58:28 -0600

    14.2.6 error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,8 +39,23 @@ router.get('/', (req, res) => {
   });
 
   router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
     res.render('login');
   });
-  
+
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  });
 
 module.exports = router;
